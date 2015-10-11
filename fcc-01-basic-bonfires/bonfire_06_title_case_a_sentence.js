e88aac96f9541b893c22b5433d89763d03b850d1
Michael Bomholt <bomholt@posteo.de>
2015-10-11 10:55:52 +0000

    + Bonfire: Title Case a Sentence

---
+++ fcc-01-basic-bonfires/bonfire_06_title_case_a_sentence.js
@@ -0,0 +1,9 @@
+function titleCase(str) {
+    var arr = str.toLowerCase().split(' ');
+    for (var i = 0; i < arr.length; i++) {
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1);
+    }
+    return arr.join(' ');
+}
+
+titleCase("I'm a little tea pot");
