baa4f4003b149a3f646c925549bd8777b9a8ca29
Michael Bomholt <bomholt@posteo.de>
2015-10-13 02:12:49 +0000

    + Bonfire: Missing letters

---
+++ fcc-03-intermediate-bonfires/bonfire_08_missing_letters.js
@@ -0,0 +1,23 @@
+function fearNotLetter(str) {
+    var arr1 = [];
+    var arr2 = [];
+    var missing = [];
+    for (var i = str.charCodeAt(0); i <= str.charCodeAt(str.length - 1); i++) {
+        arr1.push(i);
+    }
+    for (var j = 0; j < str.length; j++) {
+        arr2.push(str.charCodeAt(j));
+    }
+    for (var k = 0; k < arr1.length; k++) {
+        if (arr2.indexOf(arr1[k]) === -1) {
+            missing.push(arr1[k]);
+        }
+    }
+    if (missing.length < 1){
+        return undefined;
+    } else {
+        return String.fromCharCode.apply(null, missing);
+    }
+}
+
+fearNotLetter('abce');
