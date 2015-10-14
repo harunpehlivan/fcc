e603828810837d26ac7180ea7e06414f1cdfd58c
Michael Bomholt <bomholt@posteo.de>
2015-10-14 01:25:51 +0000

    + Bonfire: Steamroller

---
+++ fcc-03-intermediate-bonfires/bonfire_18_steamroller.js
@@ -0,0 +1,13 @@
+function steamroller(arr) {
+    var flat = [];
+    for (var i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            flat = flat.concat(steamroller(arr[i]));
+        } else {
+            flat.push(arr[i]);
+        }
+    }
+    return flat;
+}
+
+steamroller([1, [2], [3, [[4]]]]);
