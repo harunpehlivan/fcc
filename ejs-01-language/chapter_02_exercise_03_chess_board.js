0304556dd198bb97934ba80840d10774a73081e3
Michael Bomholt <bomholt@posteo.de>
2015-12-06 22:27:52 +0000

    update Exercise: Chess board

--- ejs-01-language/chapter_02_exercise_03_chess_board.js
+++ ejs-01-language/chapter_02_exercise_03_chess_board.js
@@ -1,11 +1,11 @@
 var size = 8;
 var grid = "";
-for (var y = 0; y < size; y++) {
-    for (var x = 0; x < size; x++) {
+for (var y = 1; y <= size; y++) {
+    for (var x = 1; x <= size; x++) {
         if ((x + y) % 2 === 0) {
-            grid += "#";
-        } else {
             grid += " ";
+        } else {
+            grid += "#";
         }
     }
     grid += "\n"
