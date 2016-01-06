556b0b27f75745875874c1a580678378e3ed8ce0
Michael Bomholt <bomholt@posteo.de>
2016-01-06 17:25:18 +0000

    update Bonfire: Make a Person

--- fcc-03-back-end-development/fcc-01-upper-intermediate-algorithm-scripting/bonfire_01_make_a_person.js
+++ fcc-03-back-end-development/fcc-01-upper-intermediate-algorithm-scripting/bonfire_01_make_a_person.js
@@ -1,3 +1,10 @@
+/*
+Fill in the object constructor with the methods specified in the tests.
+Those methods are getFirstName(), getLastName(), getFullName(), setFirstName(first), setLastName(last), and setFullName(firstAndLast).
+All functions that take an argument have an arity of 1, and the argument will be a string.
+These methods must be the only available means for interacting with the object.
+*/
+
 var Person = function(firstAndLast) {
     this.getFirstName = function() {
         return firstAndLast.split(' ')[0];
