998c9f65a5622f3a608adeb470597dcfabd236b4
Michael Bomholt <bomholt@posteo.de>
2016-01-07 11:02:40 +0000

    update Bonfire: Roman Numeral Converter

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_03_roman_numeral_converter.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_03_roman_numeral_converter.js
@@ -1,11 +1,16 @@
+/*
+Convert the given number into a roman numeral.
+All roman numerals answers should be provided in upper-case.
+*/
+
 function convert(num) {
-    var naturalArr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    var arr = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
     var romanArr = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     var romanStr = "";
-    for (var i = 0; i < naturalArr.length; i++) {
-        while (num >= naturalArr[i])  {
+    for (var i = 0; i < arr.length; i++) {
+        while (num >= arr[i])  {
             romanStr += romanArr[i];
-            num -= naturalArr[i];
+            num -= arr[i];
         }
     }
     return romanStr;
