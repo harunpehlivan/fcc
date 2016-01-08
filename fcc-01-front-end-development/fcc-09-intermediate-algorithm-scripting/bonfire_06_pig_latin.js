75a4c2b6e117947652305cfbd667862fd9d30e35
Michael Bomholt <bomholt@posteo.de>
2016-01-08 21:02:48 +0000

    update Bonfire: Pig Latin

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_06_pig_latin.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_06_pig_latin.js
@@ -1,3 +1,9 @@
+/*
+Translate the provided string to pig latin.
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
+If a word begins with a vowel you just add "way" to the end.
+*/
+
 function translate(str) {
     var vowels = ["a", "e", "i", "o", "u"];
     var i = 0;
