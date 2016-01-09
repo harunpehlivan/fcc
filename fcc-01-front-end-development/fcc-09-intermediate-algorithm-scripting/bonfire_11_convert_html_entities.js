ba399e118afe65c336037037896c04bc79a0f7d6
Michael Bomholt <bomholt@posteo.de>
2016-01-09 22:08:43 +0000

    update Bonfire: Convert HTML Entities

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_11_convert_html_entities.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_11_convert_html_entities.js
@@ -1,3 +1,7 @@
+/*
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a string to their corresponding HTML entities.
+*/
+
 function convert(str) {
     return str.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&apos;");
 }
