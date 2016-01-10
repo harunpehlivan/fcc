3a0c8693540aaabc23269b852200093d55fec439
Michael Bomholt <bomholt@posteo.de>
2016-01-10 19:07:25 +0000

    update Bonfire: Spinal Tap Case

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_12_spinal_tap_case.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_12_spinal_tap_case.js
@@ -1,3 +1,7 @@
+/*
+Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
+*/
+
 function spinalCase(str) {
     return str.replace(/([A-Z])/g, " $1").toLowerCase().match(/[a-z]+/g).join('-');
 }
