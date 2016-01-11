2f2b45871bc2d6331caf56dd611dcbc55fd833a9
Michael Bomholt <bomholt@posteo.de>
2016-01-11 22:48:41 +0000

    update Bonfire: Finders Keepers

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_16_finders_keepers.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_16_finders_keepers.js
@@ -1,3 +1,7 @@
+/*
+Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
+*/
+
 function find(arr, func) {
     return arr.filter(func).shift();
 }
