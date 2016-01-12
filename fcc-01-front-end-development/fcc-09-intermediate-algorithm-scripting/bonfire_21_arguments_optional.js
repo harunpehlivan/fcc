86f4d01f01f62280144107e7bd2688fef79787d3
Michael Bomholt <bomholt@posteo.de>
2016-01-12 08:54:37 +0000

    update Bonfire: Arguments Optional

---
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_21_arguments_optional.js
@@ -0,0 +1,24 @@
+/*
+Create a function that sums two arguments together.
+If only one argument is provided, then return a function that expects one argument and returns the sum.
+For example, add(2, 3) should return 5, and add(2) should return a function.
+Calling this returned function with a single argument will then return the sum:
+var sumTwoAnd = add(2);
+sumTwoAnd(3) returns 5.
+If either argument isn't a valid number, return undefined.
+*/
+
+function add() {
+    if (arguments.length === 1 && typeof arguments[0] === 'number') {
+        var x = arguments[0];
+        return function (y) {
+            if (typeof y === 'number') {
+                return x + y;
+            }
+        };
+    } else if (typeof arguments[0] === 'number' && typeof arguments[1] === 'number') {
+        return arguments[0] + arguments[1];
+    }
+}
+
+add(2,3);
