73400051710e9783e1d2523193f904b5d326098c
Michael Bomholt <bomholt@posteo.de>
2016-01-12 07:37:21 +0000

    update Bonfire: Drop it

--- fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_17_drop_it.js
+++ fcc-01-front-end-development/fcc-09-intermediate-algorithm-scripting/bonfire_17_drop_it.js
@@ -1,5 +1,13 @@
+/*
+Drop the elements of an array (first argument), starting from the front, until the predicate (second argument) returns true.
+Return the rest of the array, otherwise return an empty array.
+*/
+
 function drop(arr, func) {
-    return arr.filter(func);
+    while (!func(arr[0])) {
+        arr.shift();
+    }
+    return arr;
 }
 
 drop([1, 2, 3], function(n) {return n < 3; });
