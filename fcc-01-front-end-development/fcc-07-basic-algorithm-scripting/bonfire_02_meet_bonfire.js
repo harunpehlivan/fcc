0ad202751fec04ce368bedaff6d93b1932b246b8
Michael Bomholt <bomholt@posteo.de>
2016-01-13 17:32:07 +0000

    update Bonfire: Meet Bonfire

--- fcc-01-front-end-development/fcc-07-basic-algorithm-scripting/bonfire_02_meet_bonfire.js
+++ fcc-01-front-end-development/fcc-07-basic-algorithm-scripting/bonfire_02_meet_bonfire.js
@@ -1,3 +1,8 @@
+/*
+Your goal is to fix the failing test.
+Make this function return true no matter what.
+*/
+
 function meetBonfire(argument) {
     console.log("you can read this function's argument in the developer tools", argument);
     return true;
