84cff13ab0fdfd0430fe2dbc9983843d27012449
Michael Bomholt <bomholt@posteo.de>
2016-01-30 19:23:43 +0000

    update Looping a triangle

--- ejs_01_language/02_program_structure/01_looping_a_triangle.js
+++ ejs_01_language/02_program_structure/01_looping_a_triangle.js
@@ -1,3 +1,14 @@
+/*
+Write a loop that makes seven calls to console.log to output the following triangle:
+#
+##
+###
+####
+#####
+######
+#######
+*/
+
 for (var triangle = "#"; triangle.length <= 7; triangle += "#") {
     console.log(triangle);
 }
