2f8335a1565bcd806348b3596d607701f459400f
Michael Bomholt <bomholt@posteo.de>
2016-01-30 19:25:17 +0000

    update Chess board

--- ejs_01_language/02_program_structure/03_chess_board.js
+++ ejs_01_language/02_program_structure/03_chess_board.js
@@ -1,3 +1,18 @@
+/*
+Write a program that creates a string that represents an 8x8 grid, using newline characters to separate lines.
+At each position of the grid there is either a space or a "#" character.
+The characters should form a chess board.
+Passing this string to console.log should show something like this:
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+ # # # #
+# # # #
+*/
+
 var size = 8;
 var grid = "";
 for (var y = 1; y <= size; y++) {
