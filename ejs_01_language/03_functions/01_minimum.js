6d59d3a41b05bde0ec8c34d44fd6c37bf44d471a
Michael Bomholt <bomholt@posteo.de>
2016-01-31 09:31:28 +0000

    update Minimum

--- ejs_01_language/03_functions/01_minimum.js
+++ ejs_01_language/03_functions/01_minimum.js
@@ -1,3 +1,9 @@
+/*
+The previous chapter introduced the standard function Math.min that returns its smallest argument.
+We can do that ourselves now.
+Write a function min that takes two arguments and returns their minimum.
+*/
+
 function min(a, b) {
     if (a < b) {
         return a;
