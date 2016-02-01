20ec9c2c8370a20c276e926f4c4905c1ba26b472
Michael Bomholt <bomholt@posteo.de>
2016-02-01 10:48:25 +0000

    update Reversing an array

--- ejs_01_language/04_data_structures_objects_and_arrays/02_reversing_an_array.js
+++ ejs_01_language/04_data_structures_objects_and_arrays/02_reversing_an_array.js
@@ -1,15 +1,23 @@
+/*
+Arrays have a method reverse, which changes the array by inverting the order in which its elements appear.
+For this exercise, write two functions, reverseArray and reverseArrayInPlace.
+The first, reverseArray, takes an array as argument and produces a new array that has the same elements in the inverse order.
+The second, reverseArrayInPlace, does what the reverse method does: it modifies the array given as argument in order to reverse its elements.
+Neither may use the standard reverse method.
+*/
+
 function reverseArray(array) {
-    var newArr = [];
+    var newArray = [];
     for (var i = array.length - 1; i >= 0; i--) {
-        newArr.push(array[i]);
+        newArray.push(array[i]);
     }
-    return newArr;
+    return newArray;
 }
 function reverseArrayInPlace(array) {
     for (var i = 0; i < Math.floor(array.length / 2); i++) {
-        var oldArr = array[i];
+        var oldArray = array[i];
         array[i] = array[array.length - 1 - i];
-        array[array.length - 1 - i] = oldArr;
+        array[array.length - 1 - i] = oldArray;
     }
     return array;
 }
