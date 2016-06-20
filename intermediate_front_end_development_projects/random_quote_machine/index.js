aba8a5989c81afb798945cf64af5f6b0c191e9d5
Michael Bomholt <bomholt@posteo.de>
2016-06-20 21:35:43 +0000

    update Random Quote Machine

--- intermediate_front_end_development_projects/random_quote_machine/index.js
+++ intermediate_front_end_development_projects/random_quote_machine/index.js
@@ -1,18 +1,14 @@
 $(document).ready(function(){
   var quotes = [];
-  $.getJSON("https://raw.githubusercontent.com/bomholt/freecodecamp_solutions/master/front_end_development_certification/basic_front_end_development_projects/_assets/json/quotes.json", function(data){
+  $.getJSON("https://raw.githubusercontent.com/bomholt/freecodecamp_solutions/master/intermediate_front_end_development_projects/_assets/json/quotes.json", function(data){
     quotes = data;
     newQuote();
   });
   var i = 0;
   var j;
-
-  function getQuote(){
-    $(".text").html(quotes[i].text);
-    $(".author").html(quotes[i].author);
-    $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + quotes[i].text + " ― " + quotes[i].author);
-  }
-
+  $("#quote").on("click", function(){
+    newQuote();
+  });
   function newQuote(){
     i = Math.floor(Math.random()*quotes.length);
     if(i === j){
@@ -23,8 +19,15 @@ $(document).ready(function(){
     j = i;
     getQuote(i);
   }
-
-  $("#quote").on("click", function(){
-    newQuote();
-  });
+  function getQuote(){
+    var $image = $(".card-image");
+    MotionUI.animateIn($image, 'fade-in');
+    var $content = $(".card-content");
+    MotionUI.animateIn($content, 'fade-in');
+    $("#image").attr("src", quotes[i].image);
+    $("#text").html(quotes[i].text);
+    $("#author").html(quotes[i].author);
+    $("#episode").html(quotes[i].episode);
+    $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + "\"" + quotes[i].text + "\" - " + quotes[i].author);
+  }
 });
