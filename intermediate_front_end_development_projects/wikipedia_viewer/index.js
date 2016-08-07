3f018eaf254ba085e178c3dc4d01c0cfc175abfb
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-07 21:35:41 +0000

    update Wikipedia Viewer

--- intermediate_front_end_development_projects/wikipedia_viewer/index.js
+++ intermediate_front_end_development_projects/wikipedia_viewer/index.js
@@ -1,25 +1,48 @@
 $(document).ready(function(){
+  var input;
   $("#init-input").focus();
   $("#init-input").keyup(function(e){
-    $("#topbar-input").val(this.value);
+    $("#top-bar-input").val(this.value);
+    $("#wikipedia-init").hide();
+    $("#wikipedia-top-bar").show();
+    $("#top-bar-input").focus();
+  });
+  $("#top-bar-input").autocomplete({
+    source: function(request, response){
+      $.ajax({
+        url: "https://en.wikipedia.org/w/api.php",
+        dataType: "jsonp",
+        data: {
+          "action": "opensearch",
+          "format": "json",
+          "search": request.term
+        },
+        success: function(data){
+          response(data[1]);
+        }
+      });
+    },
+    select: function(event, ui){
+      input = ui.item.label;
+      search();
+    }
   });
   $(".search").on("click", function(){
+    input = $("#top-bar-input").val();
     search();
   });
   $("input").on("keydown", function(){
     if(event.keyCode == 13){
+      input = $("#top-bar-input").val();
       search();
     }
   });
   function search(){
     $("#wikipedia-results").empty();
-    var input = $("#topbar-input").val();
     if (input.length > 0){
-      $("#wikipedia-init").hide();
       $.getJSON("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + encodeURI(input) + "&format=json&callback=?", function(data){
         $("#wikipedia-results").html(JSON.stringify(data));
       });
-      $("#wikipedia-topbar").show();
       $("#wikipedia-results").show();
     }
   }
