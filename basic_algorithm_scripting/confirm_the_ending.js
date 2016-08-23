9d0888dd512f4a93ca7287714530f75e8316c115
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-23 15:10:17 +0000

    update Confirm the Ending

--- basic_algorithm_scripting/confirm_the_ending.js
+++
@@ -1,9 +0,0 @@
-/*
-Check if a string (first argument) ends with the given target string (second argument).
-*/
-
-function end(str, target) {
-    return str.endsWith(target);
-}
-
-end('Bastian', 'n');
