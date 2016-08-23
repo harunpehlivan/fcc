00739297c862e2153de9718ccdf8c310d07a6f7a
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-23 15:11:11 +0000

    update Repeat a string repeat a string

---
+++ basic_algorithm_scripting/08_repeat_a_string_repeat_a_string.js
@@ -0,0 +1,19 @@
+/*
+Repeat a given string (first argument) num times (second argument).
+Return an empty string if num is not a positive number.
+*/
+
+function repeatStringNumTimes(str, num){
+    var arr = [];
+    while(arr.length < num){
+        arr.push(str);
+    }
+    return arr.join("");
+}
+
+repeatStringNumTimes("*", 3);
+repeatStringNumTimes("abc", 3);
+repeatStringNumTimes("abc", 4);
+repeatStringNumTimes("abc", 1);
+repeatStringNumTimes("*", 8);
+repeatStringNumTimes("abc", -2);
