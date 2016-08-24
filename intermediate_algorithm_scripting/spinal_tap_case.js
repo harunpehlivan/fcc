572749e5bc6dabd42c753dcf10d5bc558aac9827
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-24 18:08:21 +0000

    update Spinal Tap Case

--- intermediate_algorithm_scripting/spinal_tap_case.js
+++
@@ -1,9 +0,0 @@
-/*
-Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
-*/
-
-function spinalCase(str) {
-    return str.replace(/([A-Z])/g, " $1").toLowerCase().match(/[a-z]+/g).join('-');
-}
-
-spinalCase('This Is Spinal Tap');
