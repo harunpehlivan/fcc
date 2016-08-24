398b9591daf9f96978b278e36c0e2f03689f3a01
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-24 17:05:42 +0000

    update Missing letters

--- intermediate_algorithm_scripting/08_missing_letters.js
+++ intermediate_algorithm_scripting/08_missing_letters.js
@@ -3,26 +3,29 @@ Find the missing letter in the passed letter range and return it.
 If all letters are present in the range, return undefined.
 */
 
-function fearNotLetter(str) {
+function fearNotLetter(str){
     var arr1 = [];
     var arr2 = [];
     var missing = [];
-    for (var i = str.charCodeAt(0); i <= str.charCodeAt(str.length - 1); i++) {
+    for(var i = str.charCodeAt(0); i <= str.charCodeAt(str.length - 1); i++){
         arr1.push(i);
     }
-    for (var j = 0; j < str.length; j++) {
+    for(var j = 0; j < str.length; j++){
         arr2.push(str.charCodeAt(j));
     }
-    for (var k = 0; k < arr1.length; k++) {
-        if (arr2.indexOf(arr1[k]) === -1) {
+    for(var k = 0; k < arr1.length; k++){
+        if (arr2.indexOf(arr1[k]) === -1){
             missing.push(arr1[k]);
         }
     }
-    if (missing.length < 1){
+    if(missing.length < 1){
         return undefined;
-    } else {
+    }else{
         return String.fromCharCode.apply(null, missing);
     }
 }
 
-fearNotLetter('abce');
+fearNotLetter("abce");
+fearNotLetter("abcdefghjklmno");
+fearNotLetter("bcd");
+fearNotLetter("yz");
