e145e325ab0fdc0cf0325ed25b7c6f49753d6f9c
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-24 13:03:45 +0000

    update Sum All Numbers in a Range

--- intermediate_algorithm_scripting/01_sum_all_numbers_in_a_range.js
+++ intermediate_algorithm_scripting/01_sum_all_numbers_in_a_range.js
@@ -4,16 +4,19 @@ Return the sum of those two numbers and all numbers between them.
 The lowest number will not always come first.
 */
 
-function sumAll(arr) {
+function sumAll(arr){
     var i = Math.min.apply(null, arr) + 1;
-    while (i < Math.max.apply(null, arr)) {
+    while(i < Math.max.apply(null, arr)){
         arr.push(i);
         i++;
     }
-    var sum = arr.reduce(function(a, b) {
+    var sum = arr.reduce(function(a, b){
         return a + b;
     });
     return sum;
 }
 
 sumAll([1, 4]);
+sumAll([4, 1]);
+sumAll([5, 10]);
+sumAll([10, 5]);
