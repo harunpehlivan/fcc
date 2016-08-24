0df3d13dc1e3aef812486afd5e4e237e571e308a
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-24 17:04:04 +0000

    update Pig Latin

--- intermediate_algorithm_scripting/06_pig_latin.js
+++ intermediate_algorithm_scripting/06_pig_latin.js
@@ -2,20 +2,26 @@
 Translate the provided string to pig latin.
 Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the end of the word and suffixes an "ay".
 If a word begins with a vowel you just add "way" to the end.
+Input strings are guaranteed to be English words in all lowercase.
 http://en.wikipedia.org/wiki/Pig_Latin
 */
 
-function translate(str) {
+function translatePigLatin(str){
     var vowels = ["a", "e", "i", "o", "u"];
     var i = 0;
-    while (vowels.indexOf(str.charAt(i)) < 0) {
+    while(vowels.indexOf(str.charAt(i)) < 0){
         i++;
     }
-    if (i > 0) {
+    if(i > 0){
         return str.substr(i) + str.substr(0, i) + "ay";
-    } else {
+    }else{
         return str.substr(i) + "way";
     }
 }
 
-translate("consonant");
+translatePigLatin("consonant");
+translatePigLatin("california");
+translatePigLatin("paragraphs");
+translatePigLatin("glove");
+translatePigLatin("algorithm");
+translatePigLatin("eight");
