f78a560697f95cc1811d63ff5ccac8fc3bd8b99f
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-08-24 18:12:44 +0000

    update Finders Keepers

--- intermediate_algorithm_scripting/finders_keepers.js
+++
@@ -1,9 +0,0 @@
-/*
-Create a function that looks through an array (first argument) and returns the first element in the array that passes a truth test (second argument).
-*/
-
-function find(arr, func) {
-    return arr.filter(func).shift();
-}
-
-find([1, 2, 3, 4], function(num){ return num % 2 === 0; });
