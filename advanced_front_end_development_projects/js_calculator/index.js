da716e507ddbf6a5b425c32381727e66c744b1f5
Michael Bomholt <michaelbomholt.com@gmail.com>
2016-09-17 22:53:10 +0000

    update JS Calculator

--- advanced_front_end_development_projects/js_calculator/index.js
+++ advanced_front_end_development_projects/js_calculator/index.js
@@ -1,14 +1,9 @@
 window.onload = function(){
 
-  var keys = document.getElementsByClassName("enable"),
+  var keys = document.getElementsByTagName("span"),
+      operations = ["/", "*", "-", "+"],
       hiddenResult = document.querySelectorAll(".hiddenResult h4")[0],
-      visibleResult = document.querySelectorAll(".visibleResult h2")[0],
-      del = document.getElementsByClassName("del")[0];
-
-  del.onclick = function(){
-    hiddenResult.innerHTML = "";
-    visibleResult.innerHTML = "";
-  };
+      visibleResult = document.querySelectorAll(".visibleResult h2")[0];
 
   for(var i = 0; i < keys.length; i++){
     if(keys[i].innerHTML === "="){
@@ -20,22 +15,103 @@ window.onload = function(){
 
   function addValue(i){
     return function(){
+      var lastChar = hiddenResult.innerHTML.slice(-1);
       switch(keys[i].innerHTML){
+        case ".":
+          if(lastChar.indexOf(".") > -1){
+            hiddenResult.innerHTML = hiddenResult.innerHTML.slice(0, -1);
+            visibleResult.innerHTML = visibleResult.innerHTML.slice(0, -1);
+            hiddenResult.innerHTML += ".";
+            visibleResult.innerHTML += ".";
+          }else{
+            hiddenResult.innerHTML += ".";
+            visibleResult.innerHTML += ".";
+          }
+          break;
+        case "DEL":
+          hiddenResult.innerHTML = "";
+          visibleResult.innerHTML = "";
+          break;
         case "÷":
-          hiddenResult.innerHTML += "/";
-          visibleResult.innerHTML += " ÷ ";
+          if(operations.indexOf(lastChar) > -1){
+            hiddenResult.innerHTML = hiddenResult.innerHTML.slice(0, -1);
+            visibleResult.innerHTML = visibleResult.innerHTML.slice(0, -3);
+            hiddenResult.innerHTML += "/";
+            visibleResult.innerHTML += " ÷ ";
+          }else{
+            hiddenResult.innerHTML += "/";
+            visibleResult.innerHTML += " ÷ ";
+          }
           break;
         case "×":
-          hiddenResult.innerHTML += "*";
-          visibleResult.innerHTML += " * ";
+          if(operations.indexOf(lastChar) > -1){
+            hiddenResult.innerHTML = hiddenResult.innerHTML.slice(0, -1);
+            visibleResult.innerHTML = visibleResult.innerHTML.slice(0, -3);
+            hiddenResult.innerHTML += "*";
+            visibleResult.innerHTML += " × ";
+          }else{
+            hiddenResult.innerHTML += "*";
+            visibleResult.innerHTML += " × ";
+          }
           break;
         case "-":
-          hiddenResult.innerHTML += "-";
-          visibleResult.innerHTML += " - ";
+          if(operations.indexOf(lastChar) > -1){
+            hiddenResult.innerHTML = hiddenResult.innerHTML.slice(0, -1);
+            visibleResult.innerHTML = visibleResult.innerHTML.slice(0, -3);
+            hiddenResult.innerHTML += "-";
+            visibleResult.innerHTML += " - ";
+          }else{
+            hiddenResult.innerHTML += "-";
+            visibleResult.innerHTML += " - ";
+          }
           break;
         case "+":
-          hiddenResult.innerHTML += "+";
-          visibleResult.innerHTML += " + ";
+          if(operations.indexOf(lastChar) > -1){
+            hiddenResult.innerHTML = hiddenResult.innerHTML.slice(0, -1);
+            visibleResult.innerHTML = visibleResult.innerHTML.slice(0, -3);
+            hiddenResult.innerHTML += "+";
+            visibleResult.innerHTML += " + ";
+          }else{
+            hiddenResult.innerHTML += "+";
+            visibleResult.innerHTML += " + ";
+          }
+          break;
+        case "sin":
+          hiddenResult.innerHTML += "Math.sin(";
+          visibleResult.innerHTML += "sin(";
+          break;
+        case "cos":
+          hiddenResult.innerHTML += "Math.cos(";
+          visibleResult.innerHTML += "cos(";
+          break;
+        case "tan":
+          hiddenResult.innerHTML += "Math.tan(";
+          visibleResult.innerHTML += "tan(";
+          break;
+        case "ln":
+          hiddenResult.innerHTML += "Math.log(";
+          visibleResult.innerHTML += "ln(";
+          break;
+        case "log":
+          hiddenResult.innerHTML += "Math.log10(";
+          visibleResult.innerHTML += "log(";
+          break;
+        case "!":
+
+          break;
+        case "π":
+          hiddenResult.innerHTML += "Math.PI";
+          visibleResult.innerHTML += "π";
+          break;
+        case "e":
+
+          break;
+        case "^":
+
+          break;
+        case "√":
+          hiddenResult.innerHTML += "Math.sqrt(";
+          visibleResult.innerHTML += "√(";
           break;
         default:
           hiddenResult.innerHTML += keys[i].innerHTML;
@@ -43,13 +119,10 @@ window.onload = function(){
       }
     };
   }
-
   function calculate(i){
     return function(){
-      var fix = hiddenResult.innerHTML.replace(/[\d.]+/g, function(n){
-        return parseFloat(n);
-      });
-      visibleResult.innerHTML = eval(fix);
+      var result = eval(hiddenResult.innerHTML).toFixed(11);
+      visibleResult.innerHTML = parseFloat(result);
     };
   }
 };
