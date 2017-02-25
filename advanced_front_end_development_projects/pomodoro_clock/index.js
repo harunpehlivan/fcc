585c339573c977d67465fd7ad0db7ab56094be67
Michael Bomholt <michaelbomholt.com@gmail.com>
2017-02-25 22:26:23 +0100

    update Pomodoro Clock

--- advanced_front_end_development_projects/pomodoro_clock/index.js
+++ advanced_front_end_development_projects/pomodoro_clock/index.js
@@ -1,27 +1,47 @@
 $(document).ready(function(){
-
+  var isPaused = true;
   var sessionLength = parseInt($("#session").html());
   var breakLength = parseInt($("#break").html());
+  $("#time").text(sessionLength);
+  var time = parseInt($("#time").html());
 
-  $("#play").click(function(){
+  var s = setInterval(function(){
+    if(!isPaused){
+      time--;
+      if(time === 0){
+        isPaused = true;
+      }
+      $("#time").text(time);
+    }
+  }, 1000);
+
+  $("#play").click(function(e){
     $("#play").css("display", "none");
     $(".fa").css("color", "#fff");
     $("#pause").css("display", "inline-block");
     $("#stop").css("display", "inline-block");
     $("body").css("background-color", "#ef5350");
+    e.preventDefault();
+    isPaused = false;
 	});
 
-  $("#pause").click(function(){
+  $("#pause").click(function(e){
 		$("#pause").css("display", "none");
     $("#play").css("display", "inline-block");
+    e.preventDefault();
+    isPaused = true;
 	});
 
-  $("#stop").click(function(){
+  $("#stop").click(function(e){
     $("#pause").css("display", "none");
     $("#stop").css("display", "none");
     $(".fa").css("color", "#ef5350");
     $("#play").css("display", "inline-block");
     $("body").css("background-color", "#eee");
+    e.preventDefault();
+    isPaused = true;
+    time = sessionLength;
+    $("#time").text(time);
 	});
 
 });
