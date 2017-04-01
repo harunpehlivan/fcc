a20a9d65027a1addc849b9ba7cb96da52483970a
Michael Bomholt <michaelbomholt.com@gmail.com>
2017-04-01 13:41:03 +0200

    update Simon Game

--- advanced_front_end_development_projects/simon_game/index.js
+++ advanced_front_end_development_projects/simon_game/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   var pattern = {
+    color: ["green", "red", "yellow", "blue"],
     count: 0,
-    option: ["green", "red", "yellow", "blue"],
     game: [],
     user: [],
   };
@@ -18,29 +18,26 @@ $(document).ready(function(){
     pattern.count++;
     setTimeout(function(){
       $(".counter").html(pattern.count);
-    }, 500);
-    generateGamePattern();
+    }, 1000);
   }
 
   function startGame(){
     clearGame();
+    $(".counter").html("––");
     addCount();
+    generateGamePattern();
   }
   function stopGame(){
 
   }
-  function restartGame(){
-
-  }
 
   function generateGamePattern(){
-    pattern.game.push(pattern.option[Math.floor(Math.random() * 4)]);
+    pattern.game.push(pattern.color[Math.floor(Math.random() * 4)]);
     showGamePattern();
   }
   function showGamePattern(){
     var i = 0;
     var highlight = setInterval(function(){
-      console.log(pattern.game[i]);
       switch(pattern.game[i]){
         case "green":
           $("#green").css("background-color", "#7FE283");
@@ -66,34 +63,79 @@ $(document).ready(function(){
             $("#blue").css("background-color", "#2196F3");
           }, 500);
           break;
-      };
+      }
       i++;
       if(i >= pattern.game.length){
         clearInterval(highlight);
+        turn = true;
+        $(".col").css("cursor", "pointer");
       }
     }, 1000);
     clearUser();
-    turn = true;
+  }
+
+  function checkUserPattern(){
+    if(pattern.user[pattern.user.length - 1] !== pattern.game[pattern.user.length - 1]){
+      if(strict){
+        alert("Game over!");
+        startGame();
+      }else{
+        alert("Try again!");
+        showGamePattern();
+      }
+    }else{
+      var check = pattern.game.length === pattern.user.length;
+      if(check){
+        if(pattern.count == 20){
+          alert("You won!");
+        }else{
+          addCount();
+          turn = false;
+          $(".col").css("cursor", "default");
+          generateGamePattern();
+        }
+      }
+    }
   }
 
   $("#green").click(function(){
     if(turn){
-
+      $("#green").css("background-color", "#7FE283");
+      setTimeout(function(){
+        $("#green").css("background-color", "#4CAF50");
+      }, 500);
+      pattern.user.push("green");
+      checkUserPattern();
     }
 	});
   $("#red").click(function(){
     if(turn){
-
+      $("#red").css("background-color", "#FF7669");
+      setTimeout(function(){
+        $("#red").css("background-color", "#F44336");
+      }, 500);
+      pattern.user.push("red");
+      checkUserPattern();
     }
 	});
   $("#yellow").click(function(){
     if(turn){
-
+      $("#yellow").css("background-color", "#FFFF6E");
+      setTimeout(function(){
+        $("#yellow").css("background-color", "#FFEB3B");
+      }, 500);
+      pattern.user.push("yellow");
+      checkUserPattern();
     }
 	});
   $("#blue").click(function(){
     if(turn){
-
+      $("#blue").css("background-color", "#54C9FF");
+      setTimeout(function(){
+        $("#blue").css("background-color", "#2196F3");
+      }, 500);
+      pattern.user.push("blue");
+      checkUserPattern();
     }
 	});
 
@@ -112,6 +154,13 @@ $(document).ready(function(){
     stopGame();
 	});
   $("#restart").click(function(){
-    restartGame();
+    startGame();
 	});
+  $("#strict").change(function(){
+    if($(this).is(":checked")){
+      strict = true;
+    }else{
+      strict = false;
+    }
+  });
 });
