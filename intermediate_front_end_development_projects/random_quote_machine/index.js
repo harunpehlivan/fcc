82ceb921682770c4b2f6df3c3bb0f723034093e8
Michael Bomholt <michaelbomholt.com@gmail.com>
2017-04-25 14:45:14 +0200

    update Random Quote Machine

--- intermediate_front_end_development_projects/random_quote_machine/index.js
+++ intermediate_front_end_development_projects/random_quote_machine/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var quotes = [];
-  $.getJSON("https://rawgit.com/bomholt/freecodecamp/master/intermediate_front_end_development_projects/_assets/json/quotes.json", function(data){
+  $.getJSON("https://rawgit.com/bomholt/fcc-portfolio/master/intermediate_front_end_development_projects/_assets/json/quotes.json", function(data){
     quotes = data;
     newQuote();
   });
@@ -25,7 +25,7 @@ $(document).ready(function(){
     $("#author").html(quotes[i].author);
     $("#text").html(quotes[i].text);
     $("#episode").html(quotes[i].episode);
-    $("#share").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent("https://michaelbomholt.com/freecodecamp/intermediate_front_end_development_projects/random_quote_machine/"));
+    $("#share").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent("https://michaelbomholt.com/fcc-portfolio/intermediate_front_end_development_projects/random_quote_machine/"));
     $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + "\"" + quotes[i].text + "\" - " + quotes[i].author);
     MotionUI.animateIn($image, "fade-in");
   }
