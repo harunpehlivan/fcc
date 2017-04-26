20ac74751a42c828471a9d3c569ab57f5cbff56e
Michael Bomholt <michaelbomholt.com@gmail.com>
2017-04-27 01:57:45 +0200

    update Twitch.tv Status

--- intermediate_front_end_development_projects/twitch_status/index.js
+++ intermediate_front_end_development_projects/twitch_status/index.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
   var topStreamer = ["syndicate", "riotgames", "esl_csgo", "Nightblue3", "summit1g", "LIRIK", "PhantomL0rd", "captainsparklez", "sodapoppin", "imaqtpie"];
   for(var i = 0; i < topStreamer.length; i++){
     (function(i){
-      $.getJSON("https://api.twitch.tv/kraken/streams/" + topStreamer[i] + "?client_id=cema7py8k01bs7x8g7p9dig9fk5f91e&callback=?", function(streams){
+      $.getJSON("https://api.twitch.tv/kraken/streams/" + topStreamer[i] + "?client_id=1g6nh2b43v4njh450s73ob3hd23ig1t&callback=?", function(streams){
         if(streams.status === 422){
           $(".offline-channels").show();
-          $(".offline-channels").append("<div class='col-xs-12 col-sm-6 col-md-4 channel'><a href='https://www.twitch.tv/" + topStreamer[i] + "' target='_blank' rel='noopener noreferrer'><img class='img-responsive' src='https://rawgit.com/bomholt/freecodecamp/master/intermediate_front_end_development_projects/_assets/img/twitch_closed.jpg'></a><div class='channel-info'><h4><strong><em>CHANNEL CLOSED</em></strong></h4><h5>" + topStreamer[i] + "</h5></div></div>");
+          $(".offline-channels").append("<div class='col-xs-12 col-sm-6 col-md-4 channel'><a href='https://www.twitch.tv/" + topStreamer[i] + "' target='_blank' rel='noopener noreferrer'><img class='img-responsive' src='https://rawgit.com/bomholt/fcc-portfolio/master/intermediate_front_end_development_projects/_assets/img/twitch_closed.jpg'></a><div class='channel-info'><h4><strong><em>CHANNEL CLOSED</em></strong></h4><h5>" + topStreamer[i] + "</h5></div></div>");
         }else if(streams.stream == null){
-          $.getJSON("https://api.twitch.tv/kraken/channels/" + topStreamer[i] + "?client_id=cema7py8k01bs7x8g7p9dig9fk5f91e&callback=?", function(channels){
+          $.getJSON("https://api.twitch.tv/kraken/channels/" + topStreamer[i] + "?client_id=1g6nh2b43v4njh450s73ob3hd23ig1t&callback=?", function(channels){
             $(".offline-channels").show();
             if(channels.video_banner == null){
-              $(".offline-channels").append("<div class='col-xs-12 col-sm-6 col-md-4 channel'><a href='https://www.twitch.tv/" + topStreamer[i] + "' target='_blank' rel='noopener noreferrer'><img class='img-responsive' src='https://rawgit.com/bomholt/freecodecamp/master/intermediate_front_end_development_projects/_assets/img/twitch_null.jpg'></a><div class='channel-info'><h4>" + channels.status.substring(0, 26) + "...</h4><h5>" + topStreamer[i] + "</h5></div></div>");
+              $(".offline-channels").append("<div class='col-xs-12 col-sm-6 col-md-4 channel'><a href='https://www.twitch.tv/" + topStreamer[i] + "' target='_blank' rel='noopener noreferrer'><img class='img-responsive' src='https://rawgit.com/bomholt/fcc-portfolio/master/intermediate_front_end_development_projects/_assets/img/twitch_null.jpg'></a><div class='channel-info'><h4>" + channels.status.substring(0, 26) + "...</h4><h5>" + topStreamer[i] + "</h5></div></div>");
             }else{
               $(".offline-channels").append("<div class='col-xs-12 col-sm-6 col-md-4 channel'><a href='https://www.twitch.tv/" + topStreamer[i] + "' target='_blank' rel='noopener noreferrer'><img class='img-responsive' src='" + channels.video_banner + "'></a><div class='channel-info'><h4>" + channels.status.substring(0, 26) + "...</h4><h5>" + topStreamer[i] + "</h5></div></div>");
             }
@@ -22,9 +22,9 @@ $(document).ready(function(){
       });
     })(i);
   }
-  $.getJSON("https://api.twitch.tv/kraken/streams/freecodecamp?client_id=cema7py8k01bs7x8g7p9dig9fk5f91e&callback=?", function(stream){
+  $.getJSON("https://api.twitch.tv/kraken/streams/freecodecamp?client_id=1g6nh2b43v4njh450s73ob3hd23ig1t&callback=?", function(stream){
     if(stream.stream == null){
-      $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?client_id=cema7py8k01bs7x8g7p9dig9fk5f91e&callback=?", function(channel){
+      $.getJSON("https://api.twitch.tv/kraken/channels/freecodecamp?client_id=1g6nh2b43v4njh450s73ob3hd23ig1t&callback=?", function(channel){
         $("#fcc-preview").append("<img class='img-responsive' src='" + channel.video_banner + "'>");
         $("#fcc-preview").append("<a href='" + channel.url + "' target='_blank' rel='noopener noreferrer'><div class='overlay'><i class='fa fa-play'></i></div></a>");
         $("#fcc-preview").append("<h5>offline</h5>");
@@ -59,4 +59,4 @@ $(document).ready(function(){
       }
     }
   });
-})
+});
