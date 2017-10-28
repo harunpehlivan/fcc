b2c6067840a7c8aa749e491453619e1ae98c2ac0
Michael Bomholt <michaelbomholt.com@gmail.com>
2017-10-28 23:08:07 +0200

    * FCC update

--- front_end_frameworks_projects/random_quote_machine/index.js
+++ front_end_frameworks_projects/random_quote_machine/index.js
@@ -25,7 +25,7 @@ $(document).ready(function(){
     $("#author").html(quotes[i].author);
     $("#text").html(quotes[i].text);
     $("#episode").html(quotes[i].episode);
-    $("#share").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent("https://michaelbomholt.com/fcc-portfolio/front_end_frameworks_projects/random_quote_machine"));
+    $("#share").attr("href", "https://www.facebook.com/sharer/sharer.php?u=" + encodeURIComponent("https://bomholtm.github.io/fcc/front_end_frameworks_projects/random_quote_machine/"));
     $("#tweet").attr("href", "https://twitter.com/intent/tweet?text=" + "\"" + quotes[i].text + "\" - " + quotes[i].author);
     MotionUI.animateIn($image, "fade-in");
   }
